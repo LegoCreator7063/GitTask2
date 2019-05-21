852459c2a3b67fd3b8865f9a892731bc76e913da
kaihilpatel <kaihilpatel@gmail.com>
2019-05-21 12:04:06 -0400

    kaihil fixed

--- personB2/src/personB2/personBcode.java
+++ personB2/src/personB2/personBcode.java
@@ -1,11 +1,12 @@
+import java.util.scanner //import
 public class personBcode {
 
 	public static void main(String[] args) {
 	    Scanner scan = new Scanner(System.in); 
 	    System.out.println("Type in your name. Now.");
-	    String naem = scan.nextLine();  
+	    String name = scan.nextLine();  //fixed spelling
 	    String name2 = "";
-	    for (int i = name.length() - 1; i >= 0; i++ ) {
+	    for (int i = name.length() - 1; i >= 0; i-- ) { //add --
 	    	name2 += name.charAt(i);
 	    }
 	    System.out.println("Username is: " + name2);  
